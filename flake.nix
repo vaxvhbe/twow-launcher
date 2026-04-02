edf9f5dab6683252955a37b669fd9238ee4e8e77
Xavier Van Herpe <xavier@vanherpe.me>
2026-04-02 18:13:28 +0200

    fix: set WEBKIT_EXEC_PATH to use system WebKitNetworkProcess

--- flake.nix
+++ flake.nix
@@ -162,6 +162,7 @@
             export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS"
             export APPIMAGE_SILENT_INSTALL=1
             export WINEDLLOVERRIDES="mscoree,mshtml="
+            export WEBKIT_EXEC_PATH="${pkgs.webkitgtk_4_1}/libexec/webkit2gtk-4.1"
 
             # 4. Install Icons & Desktop Entries (Self-Installing)
             mkdir -p "$HOME/.local/share/icons/hicolor/256x256/apps"
